f5e09b0cfc0f5e9addb7356cb2369020bf5dacbe
Barbie <bhernandez198@yahoo.com>
2018-11-26 21:10:03 -0500

    first attempt

---
+++ app/data/friends.js
@@ -0,0 +1,20 @@
+var friendList = [
+    {
+      name: 'Barbara Hernandez',
+      profilePic: 'https://avatars2.githubusercontent.com/u/42161307?s=460&amp;v=4',
+      scores: [
+        "4",
+        "5",
+        "5",
+        "2",
+        "4",
+        "5",
+        "4",
+        "4",
+        "3",
+        "4"
+      ]
+    }
+  ]
+  
+  module.exports = friendList;
